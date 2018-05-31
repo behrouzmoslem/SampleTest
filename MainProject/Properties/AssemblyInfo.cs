b90dbd7eb91c9a9f8f93af49b9ff16f5b4337a95
behrouzmoslem <behrouzmoslem@hotmail.com>
2018-05-31 09:45:37 +0330

    AddMainProjectAndEmptyStructure

--- MainProject/Properties/AssemblyInfo.cs
+++ MainProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MasterProject")]
+[assembly: AssemblyTitle("MainProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MasterProject")]
+[assembly: AssemblyProduct("MainProject")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1bb2dc86-d42d-4590-b486-3468f5a20ff5")]
+[assembly: Guid("5a40d3d7-41f1-40c5-ae41-a6d94ec82ce4")]
 
 // Version information for an assembly consists of the following four values:
 //
